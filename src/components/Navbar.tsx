e5fd41928352199025edff96c1a406294fedd2db
kishan lokappa <lokappakishan@gmail.com>
2025-05-07 15:42:22 +0200

    feat: Added navigation and toggle theme button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,108 +1,67 @@
+import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
+import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
+import NavLinks from './NavLinks';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [theme, setTheme] = useState<boolean>(false);
+  const handleTheme = () => {
+    setTheme(!theme);
+    console.log(theme);
+  };
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {' '}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{' '}
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+    <nav className="bg-base-200">
+      <div className="navbar max-w-6xl mx-auto p-2">
+        <div className="navbar-start">
+          {/* TITLE */}
+          <NavLink
+            to="/"
+            className="hidden lg:flex btn btn-primary text-3xl items-center "
           >
-            <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) => (isActive ? 'text-primary' : '')}
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/products"
-                className={({ isActive }) => (isActive ? 'text-primary' : '')}
-              >
-                Products
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/cart"
-                className={({ isActive }) => (isActive ? 'text-primary' : '')}
-              >
-                Cart
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/checkout"
-                className={({ isActive }) => (isActive ? 'text-primary' : '')}
-              >
-                Checkout
-              </NavLink>
-            </li>
+            C
+          </NavLink>
+          {/* DROPDOWN */}
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <FaBarsStaggered className="h-6 w-6" />
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
+            >
+              <NavLinks />
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal ">
+            <NavLinks />
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">Comfy Store</a>
+        <div className="navbar-end">
+          {/* THEME ICONS */}
+          <label className="swap">
+            <input type="checkbox" onClick={handleTheme} />
+            <div className="swap-on h-4 w-4">
+              <BsSunFill />
+            </div>
+            <div className="swap-off h-4 w-4">
+              <BsMoonFill />
+            </div>
+          </label>
+          {/* CART LINK*/}
+          <NavLink to="cart" className="btn btn-ghost btn-circle btn-md ml-4">
+            <div className="indicator">
+              <BsCart3 className="h-6 w-6" />
+              <span className="badge badge-sm badge-primary indicator-item">
+                8
+              </span>
+            </div>
+          </NavLink>
+        </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) => (isActive ? 'text-primary' : '')}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/products"
-              className={({ isActive }) => (isActive ? 'text-primary' : '')}
-            >
-              Products
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/cart"
-              className={({ isActive }) => (isActive ? 'text-primary' : '')}
-            >
-              Cart
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/checkout"
-              className={({ isActive }) => (isActive ? 'text-primary' : '')}
-            >
-              Checkout
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
-      </div>
-    </div>
+    </nav>
   );
 };
-
 export default Navbar;
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,5 @@
 const Landing = () => {
-  return <div>Landing</div>;
+  return <div className="max-w-6xl mx-auto p-2">Landing</div>;
 };
 
 export default Landing;
