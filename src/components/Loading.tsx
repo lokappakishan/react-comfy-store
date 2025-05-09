7e9509c5a54fe7fca350e501a2a89cae113a562d
kishan lokappa <lokappakishan@gmail.com>
2025-05-09 12:59:42 +0200

    feat: add Loading component in HomeLayout for page transition animation

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Loading: FC = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <span className="loading loading-ring loading-lg" />
+    </div>
+  );
+};
+
+export default Loading;
